20829f79d2436e2df4a0b16f4197221399feca0a
Hanamant001 <hanu.smalagond@gmail.com>
2025-04-13 14:18:52 +0530

    jk

---
+++ jk.tf
@@ -0,0 +1,7 @@
+resource "local_file" "adi_123" {
+  filename        = "abc/123/filefordemo.txt"
+  content         = "this file is a demo file use for creation of a local resource modification"
+  
+}
+
+
